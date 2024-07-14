04ee1e4b2243949c08afe68f0cf48e1afb0fd833
watanabe-tsubasa <t.watanabe423@gmail.com>
2024-07-15 07:37:40 +0900

    upd

--- moisen-server/src/index.ts
+++ moisen-server/src/index.ts
@@ -9,7 +9,7 @@ type Bindings = {
   CHANNEL_SECRET: string,
 }
 
-const app = new Hono<{ Bindings: Bindings}>()
+const app = new Hono<{ Bindings: Bindings }>()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
@@ -31,34 +31,63 @@ const handleEvent = async (
   event: webhook.Event,
   accessToken: string,
 ) => {
-  if (event.type !== 'message' || event.message.type !== 'text') return;
+  if (event.type !== 'message' || (event.message.type !== 'text' && event.message.type !== 'image')) return;
   if (!event.replyToken) return;
-  const { text } = event.message;
-  fetch('https://api.line.me/v2/bot/chat/loading/start', {
+
+  await fetch('https://api.line.me/v2/bot/chat/loading/start', {
     method: 'POST',
     headers: {
       "Authorization": `Bearer ${accessToken}`,
       "Content-Type": "application/json"
     },
     body: JSON.stringify({"chatId": event.source?.userId})
-  })
-  // const chat = await createChat(openaiApiKey, text);
-  // const messages = createMessages(chat) as messagingApi.Message[]
-  const responseBody: messagingApi.ReplyMessageRequest = {
-    replyToken: event.replyToken,
-    messages: [
-      {'type': 'text', 'text': text}
-    ] 
+  });
+
+  let responseBody: messagingApi.ReplyMessageRequest;
+
+  if (event.message.type === 'image') {
+    responseBody = {
+      replyToken: event.replyToken,
+      messages: [
+        {
+          'type': 'template',
+          'altText': 'phone announce',
+          'template': {
+            'type': 'confirm',
+            'text': 'AI林さんからのお電話です',
+            'actions': [
+              {
+                'type': 'uri', // or 'message' depending on your use case
+                'label': '電話に出る',
+                'uri': 'https://240714-moisen-2.pages.dev/'
+              },
+              {
+                'type': 'postback', // Adding a cancel action
+                'label': 'キャンセル',
+                'data': 'action=cancel'
+              }
+            ]
+          }
+        }
+      ] 
+    };
+  } else {
+    responseBody = {
+      replyToken: event.replyToken,
+      messages: [
+        {'type': 'text', 'text': '問診票をご準備できたら写真を撮って送信をお願いします'}
+      ] 
+    };
   }
-  // `${chat.storeList}`
-  return fetch('https://api.line.me/v2/bot/message/reply', {
+
+  await fetch('https://api.line.me/v2/bot/message/reply', {
     method: 'POST',
     headers: {
       "Authorization": `Bearer ${accessToken}`,
       "Content-Type": "application/json"
     },
     body: JSON.stringify(responseBody)
-  })
+  });
 }
 
 export default app
